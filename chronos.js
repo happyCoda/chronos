659a2cbaef8f960969ac5b6bdd8b6393daab8dc4
happyCoda <ypworkspace@gmail.com>
2016-02-04 10:11:16 +0300

    adjustUnits method bug fixed

--- chronos.js
+++ chronos.js
@@ -1,132 +1,135 @@
 /*
-* chronos.js
-*
-* Created by happyCoda on 20.10.2015.
-* Copyright (c) 2015 happyCoda. All rights reserved.
-*/
+ * chronos.js
+ *
+ * Created by happyCoda on 20.10.2015.
+ * Copyright (c) 2015 happyCoda. All rights reserved.
+ */
 
 define(function () {
-	'use strict';
+    'use strict';
 
-	var Chronos = {};
+    var Chronos = {};
 
-	Chronos.get = function get(prop) {
-		return this[prop];
-	};
+    Chronos.get = function get(prop) {
+        return this[prop];
+    };
 
-	Chronos.getCurrentTime = function getCurrentTime() {
+    Chronos.getCurrentTime = function getCurrentTime() {
 
-		var currentTime = new Date();
+        var currentTime = new Date();
 
-		return currentTime;
+        return currentTime;
 
-	};
+    };
 
-	Chronos.getFutureTime = function getFutureTime() {
+    Chronos.getFutureTime = function getFutureTime() {
 
-		if (!this.futureTime) {
-			this.futureTime = new Date(this.timeString);
-		}
+        if (!this.futureTime) {
+            this.futureTime = new Date(this.timeString);
+        }
 
-		return this.futureTime;
-	};
+        return this.futureTime;
+    };
 
-	Chronos.getDiff = function getDiff() {
+    Chronos.getDiff = function getDiff() {
 
-		var diff;
+        var diff;
 
-		diff = (this.getFutureTime().getTime() - this.getCurrentTime().getTime());
+        diff = (this.getFutureTime().getTime() - this.getCurrentTime().getTime());
 
-		return diff;
+        return diff;
 
-	};
+    };
 
-	Chronos.calculateUnits = function calculateUnits(timestamp) {
+    Chronos.calculateUnits = function calculateUnits(timestamp) {
 
-		var hoursRaw,
-		hoursRemains,
-		minutesRaw,
-		minutesRemains,
-		secondsRaw;
+        var hoursRaw,
+            hoursRemains,
+            minutesRaw,
+            minutesRemains,
+            secondsRaw;
 
-		this.unitObj = {};
+        this.unitObj = {};
 
-		hoursRaw = timestamp / 10000 / 3600;
+        hoursRaw = timestamp / 1000 / 3600;
 
-		this.unitObj.hours = Math.floor(hoursRaw);
+        this.unitObj.hours = Math.floor(hoursRaw);
 
-		hoursRemains = hoursRaw - this.unitObj.hours;
+        hoursRemains = hoursRaw - this.unitObj.hours;
 
-		minutesRaw = hoursRemains * 60;
+        minutesRaw = hoursRemains * 60;
 
-		this.unitObj.minutes = Math.floor(minutesRaw);
+        this.unitObj.minutes = Math.floor(minutesRaw);
 
-		minutesRemains = minutesRaw - this.unitObj.minutes;
+        minutesRemains = minutesRaw - this.unitObj.minutes;
 
-		secondsRaw = minutesRemains * 60;
+        secondsRaw = minutesRemains * 60;
 
-		this.unitObj.seconds = Math.floor(secondsRaw);
+        this.unitObj.seconds = Math.floor(secondsRaw);
 
-		return this.unitObj;
+        return this.unitObj;
 
-	};
+    };
 
-	Chronos.adjustUnits = function adjustUnits() {
+    Chronos.adjustUnits = function adjustUnits() {
 
-		var props, unitObj;
+        var props, unitObj;
 
-		props = Object.keys(this.unitObj);
+        props = Object.keys(this.unitObj);
 
-		unitObj = this.unitObj;
+        unitObj = this.unitObj;
 
-		props.forEach(function (prop) {
-			if (unitObj[prop].length < 2) {
-				unitObj[prop] = '0' + unitObj[prop];
-			}
-		});
+        props.forEach(function (prop) {
 
-	};
+            unitObj[prop] = (unitObj[prop]).toString();
 
-	Chronos.composeString = function composeString() {
+            if (unitObj[prop].length < 2) {
+                unitObj[prop] = '0' + unitObj[prop];
+            }
+        });
 
-		this.composedString = this.unitObj.hours + ':' + this.unitObj.minutes + ':' + this.unitObj.seconds;
+    };
 
-		return this.composedString;
+    Chronos.composeString = function composeString() {
 
-	};
+        this.composedString = this.unitObj.hours + ':' + this.unitObj.minutes + ':' + this.unitObj.seconds;
 
-	Chronos.composedToArray = function composedToArray(composedString) {
+        return this.composedString;
 
-		if (this.composedString) {
-			this.composedArray = this.composedString.split(':');
-		} else {
-			this.composedArray = this.composeString().split(':');
-		}
+    };
 
-		return this.composedArray;
+    Chronos.composedToArray = function composedToArray(composedString) {
 
-	};
+        if (this.composedString) {
+            this.composedArray = this.composedString.split(':');
+        } else {
+            this.composedArray = this.composeString().split(':');
+        }
 
-	Chronos.controller = function controller() {
-		var diff;
+        return this.composedArray;
 
-		diff = this.getDiff();
+    };
 
-		this.calculateUnits(diff);
+    Chronos.controller = function controller() {
+        var diff;
 
-		this.adjustUnits();
+        diff = this.getDiff();
 
-		return this;
-	};
+        this.calculateUnits(diff);
 
-	Chronos.start = function start(timeString) {
-		this.timeString = timeString;
+        this.adjustUnits();
 
-		this.controller();
+        return this;
+    };
 
-		return this;
+    Chronos.start = function start(timeString) {
+        this.timeString = timeString;
 
-	};
+        this.controller();
 
-	return Chronos;
+        return this;
+
+    };
+
+    return Chronos;
 });
